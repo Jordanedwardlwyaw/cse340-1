e56dc7560e4a4840538e8aa6d35952a8ea17d7da
RoikejuniorHaringanji <roikejunior07@gmail.com>
2025-10-05 21:13:20 +0300

    Sunday

--- server.js
+++ server.js
@@ -2,27 +2,58 @@
  * This server.js file is the primary file of the 
  * application. It is used to control the project.
  *******************************************/
-const expressLayouts = require("express-ejs-layouts")
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-const staticRoutes = require("./routes/static")
+const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/")
+
 /* ***********************
- * Routes
+ * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
-// Use static routes if needed
-app.use(staticRoutes)
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
 
 // Index route
-app.get("/", function(req, res){
-    res.render("index", {title: "Home"})
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// Inventory routes
+app.use("/inventory", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if (err.status == 404) {
+    message = err.message
+  } else {
+    message = "Oh no! There was a crash. Maybe try a different route?"
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  })
 })
 
 /* ***********************
