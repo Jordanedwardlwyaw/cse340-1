0b7696dc47c128b37876d7054e740b179c72001c
RoikejuniorHaringanji <roikejunior07@gmail.com>
2025-10-14 14:59:41 +0300

    wk 456

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,14 +1,35 @@
-INSERT INTO account (account_firstname, account_lastname, account_email, account_password) Values ('Tony', 'Stark', 'tony@starkent.com', ' Iam1ronM@n');
+-- Insert new record for Tony Stark
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+-- Update Tony Stark's account_type to Admin
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+-- Delete Tony Stark's record from the account table
+DELETE FROM account
+WHERE account_email = 'tony@starkent.com';
 
-UPDATE account SET account_type = 'Admin' WHERE account_id = 1;
+-- Update the GM Hummer description from 'small interiors' to 'a huge interior'
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'the small interiors', ' a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
 
-DELETE FROM account WHERE account_id = 1;
+-- Select make and model from inventory and classification name from classification table for "Sport" category
+SELECT 
+    inventory.inv_make, 
+    inventory.inv_model, 
+    classification.classification_name
+FROM 
+    inventory
+INNER JOIN 
+    classification
+ON 
+    inventory.classification_id = classification.classification_id
+WHERE 
+    classification.classification_name = 'Sport';
 
-UPDATE inventory SET inv_description = replace(inv_description, 'small interiors', 'a huge interior') WHERE inv_id = 10;
-
-SELECT inv_make, inv_model, c.classification_name
-FROM inventory i
-INNER JOIN classification c ON i.classification_id = c.classification_id
-WHERE i.classification_id = 2;
-
-UPDATE inventory SET inv_image=replace(inv_image,'/images','/images/vehicles'), inv_thumbnail=replace(inv_thumbnail, '/images', '/images/vehicles');
+-- Remove '/images/vehicles/vehiclesvehicles' from inv_image and inv_thumbnail paths then replacing with /images/vehicles/
+UPDATE inventory
+SET 
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
