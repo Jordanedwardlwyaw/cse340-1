299629efcc32edb1657e991fb4670796a70bf227
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-11-24 15:07:42 +0300

    add

--- app.js
+++ app.js
@@ -6,33 +6,32 @@ const invRoutes = require("./routes/invRoutes");
 const accountRoutes = require("./routes/accountRoutes");
 const errorMiddleware = require("./utilities/errors");
 
-
 // View Engine
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-
 // Static Files
 app.use(express.static(path.join(__dirname, "public")));
-
 
 // Body Parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 
 // Routes
 app.use("/", baseRoutes);
 app.use("/inv", invRoutes);
 app.use("/account", accountRoutes);
 
-
 // 404 Handler
 app.use(errorMiddleware.handle404);
-
 
 // Error Handler
 app.use(errorMiddleware.handleErrors);
 
+const port = process.env.PORT || 3000;
 
-module.exports = app;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+module.exports = app; // Optional if you still want to export for testing
