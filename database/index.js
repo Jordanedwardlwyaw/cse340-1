0a7d94e26b78b8cd91169084e8551e839bcded88
Jordanedwardlwyaw <jlwaya@byupathwy.edu>
2025-11-25 16:06:16 +0300

    add

--- database/index.js
+++ database/index.js
@@ -7,6 +7,9 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
   password: String(process.env.PG_PASSWORD),
   port: Number(process.env.PG_PORT),
+  ssl: {
+    rejectUnauthorized: false, // required for Render.com
+  },
 });
 
 // Test the connection
